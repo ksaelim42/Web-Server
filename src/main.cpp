0c3e2d0aaa39e41899dac529a4d9082ac955b94d
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2024-03-04 14:33:16 +0700

    created fucntion: scanStartLine

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: prachman <prachman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 11:10:00 by prachman          #+#    #+#             */
-/*   Updated: 2024/03/04 13:17:51 by prachman         ###   ########.fr       */
+/*   Updated: 2024/03/04 14:31:49 by prachman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ httpReq	storeReq(std::string rawReq)
 	}
 	if (headVec.size() != tailVec.size())
 	{
-		std::cout << "Error: Something is wrong when trying to store the request" << std::endl;
+		std::cout << "Error: Something went wrong when trying to store the request" << std::endl;
 		exit(1);
 	}
 	for (int i = 0; i < headVec.size(); i++) 
@@ -62,15 +62,37 @@ httpReq	storeReq(std::string rawReq)
 	return reqData;
 }
 
+int	scanStartLine(std::map<std::string, std::string> startLine)
+{
+	std::string	methods[] = {"GET", "POST", "DELETE"};
+	bool	isMethod = false;
+	
+	for (int i = 0; i < 3; i++)
+	{
+		if (startLine["method"] == methods[i])
+			isMethod = true;
+	}
+	if (!isMethod)
+		return (std::cout << "Incorrect HTTP Method: " << startLine["method"] << std::endl, 400);
+	if (startLine["version"] != "HTTP/1.1")
+		return (std::cout << "Incorrect HTTP version: " << startLine["version"] << std::endl, 400);
+	return 0;
+}
+
 int	main()
 {
 	httpReq		repData;
+	int			status;
+
 	std::string	rawReq = "GET /favicon.ico HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nsec-ch-ua: ""Not A(Brand"";v=""99"", ""Google Chrome"";v=""121"", ""Chromium"";v=""121""\r\nsec-ch-ua-mobile: ?0\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36\r\nsec-ch-ua-platform: ""Windows""\r\nAccept: image/avif,image/webp,image/apng,image/svg+xml,image/,/*;q=0.8\r\nSec-Fetch-Site: same-origin\r\nSec-Fetch-Mode: no-cors\r\nSec-Fetch-Dest: image\r\nReferer: http://localhost:8080/\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9,th;q=0.8\r\n";
 	repData= storeReq(rawReq);
-
+	status = scanStartLine(repData.startLine);
+	if (!status)
+		std::cout << "ok!" << std::endl;
+	else
+		std::cout << status << std::endl;
 	// for (std::map<std::string, std::string>::iterator it = repData.startLine.begin(); it != repData.startLine.end(); it++)
 	// 	std::cout << it->first << " => " << it->second << std::endl;
 	// for (std::map<std::string, std::string>::iterator it = repData.headers.begin(); it != repData.headers.end(); it++)
 	// 	std::cout << it->first << " => " << it->second << std::endl;
-	
 }
